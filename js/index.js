2d780f1a15e92046e523d26dba84583d5c7bfcf5
lgnacio Armand-ugón <57232767+IgnacioArmandUgon@users.noreply.github.com>
2022-02-09 10:56:35 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,30 @@
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+
+function sumarArray(arreglo){
+    let resultado= 0;
+    arreglo.array.forEach(element => {
+        resultado += element;
+    });
+    console.log(resultado);
+}
+
+var a = [1,2,3,4];
+sumarArray(a);
+
 (function(){
-    console.log("xd")
-}())
+    const escalones = 4;
+    var salto = 2;
+    var posibilidad = [];
+    var posibilidades = [];
+
+    for(var i = 0; i< n; i++){
+        
+        let numero = getRandomArbitrary(0, k);
+
+        
+
+    }
+    
+}());
